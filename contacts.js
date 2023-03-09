d1c2cc41b4462c757f6ebee49caf53612f9d5c35
Liakhov-Serg <112199980+Liakhov-Serg@users.noreply.github.com>
2023-03-09 22:44:29 +0200

    create
    create

---
+++ contacts.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+const contactsPath = path.join(__dirname, "./db/contacts.js")
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
